d15dd3235df142a23e54e7590e1731128df951a5
Jack Ng <jack041395@gmail.com>
2019-04-30 14:42:25 +0800

    Update Network.py

--- Network.py
+++ Network.py
@@ -25,17 +25,17 @@ def res_block_gen(model, kernal_size, filters, strides):
     gen = model
     
     model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    model = BatchNormalization(momentum = 0.5)(model)
+    #model = BatchNormalization(momentum = 0.5)(model)
     # Using Parametric ReLU
     model = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(model)
     model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    model = BatchNormalization(momentum = 0.5)(model)
+    #model = BatchNormalization(momentum = 0.5)(model)
         
     model = add([gen, model])
     
     return model
     
-    
+
 def up_sampling_block(model, kernal_size, filters, strides):
     
     # In place of Conv2D and UpSampling2D we can also use Conv2DTranspose (Both are used for Deconvolution)
@@ -77,12 +77,12 @@ class Generator(object):
 	        model = res_block_gen(model, 3, 64, 1)
 	    
 	    model = Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same")(model)
-	    model = BatchNormalization(momentum = 0.5)(model)
+	    #model = BatchNormalization(momentum = 0.5)(model)
 	    model = add([gen_model, model])
 	    
 	    # Using 2 UpSampling Blocks
-	    for index in range(2):
-	        model = up_sampling_block(model, 3, 256, 1)
+	    #for index in range(2):
+	    #    model = up_sampling_block(model, 3, 256, 1)
 	    
 	    model = Conv2D(filters = 3, kernel_size = 9, strides = 1, padding = "same")(model)
 	    model = Activation('tanh')(model)
