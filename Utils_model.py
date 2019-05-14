f92656ae26fee123056e737c021f1d639adbffe8
Jack Ng <jack041395@gmail.com>
2019-05-14 09:33:51 +0800

    Update Utils_model.py

--- Utils_model.py
+++ Utils_model.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-#title           :Utils_model.py
-#description     :Have functions to get optimizer and loss
-#author          :Deepak Birla
-#date            :2018/10/30
-#usage           :imported in other files
-#python_version  :3.5.4
-
 from keras.applications.vgg19 import VGG19
 import keras.backend as K
 from keras.models import Model
@@ -17,12 +9,10 @@ class VGG_LOSS(object):
         
         self.image_shape = image_shape
 
-    # computes VGG loss or content loss
     def vgg_loss(self, y_true, y_pred):
     
         vgg19 = VGG19(include_top=False, weights='imagenet', input_shape=self.image_shape)
         vgg19.trainable = False
-        # Make trainable as False
         for l in vgg19.layers:
             l.trainable = False
         model = Model(inputs=vgg19.input, outputs=vgg19.get_layer('block5_conv4').output)
