e5cc75e0e40cbfdce07ec4dfc4c81810f3d25cfa
Jack Ng <jack041395@gmail.com>
2019-05-14 10:53:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-#title           :test.py
-#description     :to test the model
-#author          :Deepak Birla
-#date            :2018/10/30
-#usage           :python test.py --options
-#python_version  :3.5.4 
-
 from keras.models import Model
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -21,27 +13,25 @@ import argparse
 import Utils, Utils_model
 from Utils_model import VGG_LOSS
 
-image_shape = (96,96,3)
+image_shape = (384, 384, 3) # shape
 
-def test_model(input_hig_res, model, number_of_images, output_dir):
-    
-    x_test_lr, x_test_hr = Utils.load_test_data_for_model(input_hig_res, 'jpg', number_of_images)
-    Utils.plot_test_generated_images_for_model(output_dir, model, x_test_hr, x_test_lr)
+def test_model(input_hdr, model, number_of_images, output_dir):
+    x_test_sdr, x_test_hdr = Utils.load_test_data_for_model(input_hdr, 'jpg', number_of_images)
+    Utils.plot_test_generated_images_for_model(output_dir, model, x_test_hdr, x_test_sdr)
 
-def test_model_for_lr_images(input_low_res, model, number_of_images, output_dir):
-
-    x_test_lr = Utils.load_test_data(input_low_res, 'jpg', number_of_images)
-    Utils.plot_test_generated_images(output_dir, model, x_test_lr)
+def test_model_for_sdr_images(input_sdr, model, number_of_images, output_dir):
+    x_test_sdr = Utils.load_test_data(input_sdr, 'jpg', number_of_images)
+    Utils.plot_test_generated_images(output_dir, model, x_test_sdr)
 
 if __name__== "__main__":
 
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('-ihr', '--input_hig_res', action='store', dest='input_hig_res', default='./data/' ,
-                    help='Path for input images Hig resolution')
+    parser.add_argument('-ihdr', '--input_hdr', action='store', dest='input_hdr', default='./data_hdr/' ,
+                    help='Path for input HDR images')
                     
-    parser.add_argument('-ilr', '--input_low_res', action='store', dest='input_low_res', default='./data_lr/' ,
-                    help='Path for input images Low resolution')
+    parser.add_argument('-isdr', '--input_sdr', action='store', dest='input_sdr', default='./data_sdr/' ,
+                    help='Path for input SDR images')
                     
     parser.add_argument('-o', '--output_dir', action='store', dest='output_dir', default='./output/' ,
                     help='Path for Output images')
@@ -61,14 +51,10 @@ if __name__== "__main__":
     model = load_model(values.model_dir , custom_objects={'vgg_loss': loss.vgg_loss})
     
     if values.test_type == 'test_model':
-        test_model(values.input_hig_res, model, values.number_of_images, values.output_dir)
+        test_model(values.input_hdr, model, values.number_of_images, values.output_dir)
         
-    elif values.test_type == 'test_lr_images':
-        test_model_for_lr_images(values.input_low_res, model, values.number_of_images, values.output_dir)
+    elif values.test_type == 'test_sdr_images':
+        test_model_for_sdr_images(values.input_sdr, model, values.number_of_images, values.output_dir)
         
     else:
         print("No such option")
-
-
-
-
