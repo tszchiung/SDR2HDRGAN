57c9d3e4cfecb3358f1282cd95478a3b7362d8c1
Jack Ng <jack041395@gmail.com>
2019-05-14 09:31:45 +0800

    Update Network.py

--- Network.py
+++ Network.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-#title           :Network.py
-#description     :Architecture file(Generator and Discriminator)
-#author          :Deepak Birla
-#date            :2018/10/30
-#usage           :from Network import Generator, Discriminator
-#python_version  :3.5.4 
-
-# Modules
 from keras.layers import Dense
 from keras.layers.core import Activation
 from keras.layers.normalization import BatchNormalization
@@ -19,18 +10,12 @@ from keras.layers.advanced_activations import LeakyReLU, PReLU
 from keras.layers import add
 
 
-# Residual block
 def res_block_gen(model, kernal_size, filters, strides):
-    
-    gen = model
-    
+	
+    gen = model  
     model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    #model = BatchNormalization(momentum = 0.5)(model)
-    # Using Parametric ReLU
     model = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(model)
-    model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    #model = BatchNormalization(momentum = 0.5)(model)
-        
+    model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)       
     model = add([gen, model])
     
     return model
@@ -38,8 +23,6 @@ def res_block_gen(model, kernal_size, filters, strides):
 
 def up_sampling_block(model, kernal_size, filters, strides):
     
-    # In place of Conv2D and UpSampling2D we can also use Conv2DTranspose (Both are used for Deconvolution)
-    # Even we can have our own function for deconvolution (i.e one made in Utils.py)
     #model = Conv2DTranspose(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
     model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
     model = UpSampling2D(size = 2)(model)
@@ -56,7 +39,7 @@ def discriminator_block(model, filters, kernel_size, strides):
     
     return model
 
-# Network Architecture is same as given in Paper https://arxiv.org/pdf/1609.04802.pdf
+
 class Generator(object):
 
     def __init__(self, noise_shape):
@@ -72,17 +55,11 @@ class Generator(object):
 	    
 	    gen_model = model
         
-        # Using 16 Residual Blocks
 	    for index in range(16):
 	        model = res_block_gen(model, 3, 64, 1)
 	    
 	    model = Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same")(model)
-	    #model = BatchNormalization(momentum = 0.5)(model)
 	    model = add([gen_model, model])
-	    
-	    # Using 2 UpSampling Blocks
-	    #for index in range(2):
-	    #    model = up_sampling_block(model, 3, 256, 1)
 	    
 	    model = Conv2D(filters = 3, kernel_size = 9, strides = 1, padding = "same")(model)
 	    model = Activation('tanh')(model)
@@ -91,7 +68,7 @@ class Generator(object):
         
 	    return generator_model
 
-# Network Architecture is same as given in Paper https://arxiv.org/pdf/1609.04802.pdf
+
 class Discriminator(object):
 
     def __init__(self, image_shape):
